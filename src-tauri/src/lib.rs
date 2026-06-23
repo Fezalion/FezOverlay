ef7f9db2a4f9f99ad0812e6ea3e4f3fcdc2ab532
Fezalion <kerimgileren@gmail.com>
2026-06-23 23:40:52 +0300

    feat: we gon try autoupdating

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -17,6 +17,7 @@ use config::AppConfig;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
             // When a second instance is launched, bring the main window to the front
             if let Some(window) = app.get_webview_window("main") {
